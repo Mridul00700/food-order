7824428543f03b75e2b3a64369919e1a5b92051f
Mridul00700 <55996869+Mridul00700@users.noreply.github.com>
2021-09-08 20:43:36 +0530

    Structure-Project

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,19 @@
+
+
+import './Header.module.css';
+
+const Header = props => {
+    return (
+        <>
+            <header>
+                <h1>ReactMeals</h1>
+                <button>Cart</button>
+            </header>
+            <div>
+                <img />
+            </div>
+        </>
+    )
+}
+
+export default Header;
