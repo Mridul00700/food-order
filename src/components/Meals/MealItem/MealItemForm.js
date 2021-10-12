51d9d2638d958122b47c8897f646744ec07a0d88
Mridul00700 <55996869+Mridul00700@users.noreply.github.com>
2021-10-12 13:33:57 +0530

    forward_ref

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,16 +1,29 @@
 import classes from './MealItemForm.module.css';
 import Input from '../../UI/Input';
+import { useRef } from 'react';
+
+
 
 const MealItemForm = props => {
-    return <form className={classes.form}>
-        <Input label="Amount" input={{
-            id: 'amount' + props.id,
-            type: 'number',
-            min: '1',
-            max: '5',
-            step: '1',
-            defaultValue: '1'
-        }} />
+    const amountInputRef = useRef();
+
+    const submitHandler = event => {
+        event.preventDefault();
+    }
+
+
+    return <form className={classes.form} onSubmit={submitHandler} >
+        <Input
+            label="Amount"
+            ref={amountInputRef}
+            input={{
+                id: 'amount' + props.id,
+                type: 'number',
+                min: '1',
+                max: '5',
+                step: '1',
+                defaultValue: '1'
+            }} />
         <button>+ ADD</button>
     </form>
 }
--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -1,11 +1,12 @@
 import classes from './Input.module.css';
+import React from 'react';
 
 // {...props.input} give all the property in object to the input as property i.e. - type = "text"   if props.input has {type: "text"}
-const Input = props => {
+const Input = React.forwardRef((props, ref) => {
     return <div className={classes.input}>
         <label htmlFor={props.input.id}>{props.label}</label>
-        <input {...props.input} />
+        <input ref={ref} {...props.input} />
     </div>
-}
+})
 
 export default Input;
