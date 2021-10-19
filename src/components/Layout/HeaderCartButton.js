09eb933d4550e9e101355ee1e089941bef6557a5
Mridul00700 <55996869+Mridul00700@users.noreply.github.com>
2021-10-19 11:25:28 +0530

    Pop up the cart button-functionalty

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -2,17 +2,39 @@ import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css';
 import { useContext } from 'react';
 import CartContext from '../../store/cart-context';
+import { useState, useEffect } from 'react';
 
 const HeaderCartButton = (props) => {
 
     const cartCtx = useContext(CartContext);
 
-    const numberOfCartItems = cartCtx.items.reduce((currentValue, item) => {
+    const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
+
+    const { items } = cartCtx
+
+    useEffect(() => {
+        if (items.length === 0) {
+            return;
+        }
+
+        setBtnIsHighlighted(true);
+        const timer = setTimeout(() => {
+            setBtnIsHighlighted(false)
+        }, 300);
+
+        return () => {
+            clearTimeout(timer);
+        }
+    }, [items])
+
+    const numberOfCartItems = items.reduce((currentValue, item) => {
         return currentValue + item.amount
     }, 0);
 
+
+    const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`
     return (
-        <button className={classes.button} onClick={props.onClick}>
+        <button className={btnClasses} onClick={props.onClick}>
             <span className={classes.icon}>
                 <CartIcon />
             </span>
